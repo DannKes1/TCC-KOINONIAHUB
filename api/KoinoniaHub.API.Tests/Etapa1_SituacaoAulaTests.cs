d33dd4734d7ed42fb621b3ec9934e4ed5e3dd251
Dannkes <burst768@gmail.com>
2026-09-25 00:56:50 -0400

    Testes da situação da aula com contexto SQLite em memória

---
+++ api/KoinoniaHub.API.Tests/Etapa1_SituacaoAulaTests.cs
@@ -0,0 +1,68 @@
+﻿using KoinoniaHub.API.Dominio.Entidades;
+using KoinoniaHub.API.Tests.Infraestrutura;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoinoniaHub.API.Tests
+{
+
+    public class Etapa1_SituacaoAulaTests
+    {
+        [Fact]
+        public async Task Aula_NovaSemInformarSituacao_PersisteComoEmAberto()
+        {
+            using var ctx = new ContextoDeTeste();
+
+            var aula = NovaAulaMinima();
+            ctx.Db.Aulas.Add(aula);
+            await ctx.Db.SaveChangesAsync();
+
+            using var leitura = ctx.NovoContexto();
+            var lida = await leitura.Aulas.SingleAsync(a => a.Id == aula.Id);
+
+            Assert.Equal(SituacaoAula.EmAberto, lida.Situacao);
+        }
+
+        [Theory]
+        [InlineData(SituacaoAula.Consolidada)]
+        [InlineData(SituacaoAula.NaoRealizada)]
+        public async Task Aula_ComSituacaoAlterada_PersisteOValorGravado(string situacao)
+        {
+            using var ctx = new ContextoDeTeste();
+
+            var aula = NovaAulaMinima();
+            ctx.Db.Aulas.Add(aula);
+            await ctx.Db.SaveChangesAsync();
+
+            aula.Situacao = situacao;
+            await ctx.Db.SaveChangesAsync();
+
+            using var leitura = ctx.NovoContexto();
+            var lida = await leitura.Aulas.SingleAsync(a => a.Id == aula.Id);
+
+            Assert.Equal(situacao, lida.Situacao);
+        }
+
+        [Fact]
+        public void SituacaoAula_ExpoeExatamenteOsTresValoresDaMonografia()
+        {
+            Assert.Equal(
+                new[] { "EmAberto", "Consolidada", "NaoRealizada" },
+                SituacaoAula.Todas);
+        }
+
+        private static Aula NovaAulaMinima()
+        {
+            var igreja = new Igreja { Nome = "Igreja de Teste" };
+            var departamento = new Departamento { Nome = "Turma de Teste", Igreja = igreja };
+            var materia = new Materia { Nome = "Matéria de Teste", Departamento = departamento };
+            var professor = new Pessoa { Nome = "Professor de Teste", Igreja = igreja };
+
+            return new Aula
+            {
+                Data = DateTime.UtcNow,
+                Materia = materia,
+                Professor = professor
+            };
+        }
+    }
+}
---
+++ api/KoinoniaHub.API.Tests/Infraestrutura/ContextoDeTeste.cs
@@ -0,0 +1,43 @@
+﻿using KoinoniaHub.API.Infraestrutura.Dados;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoinoniaHub.API.Tests.Infraestrutura
+{
+    
+    public sealed class ContextoDeTeste : IDisposable
+    {
+        private readonly SqliteConnection _conexao;
+
+        public KoinoniaHubDbContext Db { get; }
+
+        public ContextoDeTeste()
+        {
+            _conexao = new SqliteConnection("DataSource=:memory:");
+            _conexao.Open();
+
+            var opcoes = new DbContextOptionsBuilder<KoinoniaHubDbContext>()
+                .UseSqlite(_conexao)
+                .Options;
+
+            Db = new KoinoniaHubDbContext(opcoes);
+            Db.Database.EnsureCreated();
+        }
+
+     
+        public KoinoniaHubDbContext NovoContexto()
+        {
+            var opcoes = new DbContextOptionsBuilder<KoinoniaHubDbContext>()
+                .UseSqlite(_conexao)
+                .Options;
+
+            return new KoinoniaHubDbContext(opcoes);
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+            _conexao.Dispose();
+        }
+    }
+}
