28a1e64ceea21889586530c9b1c0e0a0119c2e8f
Dannkes <burst768@gmail.com>
2026-09-26 15:51:44 -0400

    Adiciona entidade AceiteTermo e constantes de meio de aceite (RF42/RNF 42.5)

---
+++ api/KoinoniaHub.API/Dominio/Entidades/Models/AceiteTermo.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KoinoniaHub.API.Dominio.Entidades
+{
+    public class AceiteTermo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("Igreja")]
+        public int IgrejaId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string TermoVersao { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(64)]
+        public string TermoHash { get; set; } = string.Empty;
+
+        public DateTime AceitoEm { get; set; }
+
+        [StringLength(45)]
+        public string? Ip { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Meio { get; set; } = string.Empty;
+
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+
+        public Usuario Usuario { get; set; } = null!;
+        public Igreja Igreja { get; set; } = null!;
+    }
+}
---
+++ api/KoinoniaHub.API/Dominio/Entidades/Models/MeioAceiteTermo.cs
@@ -0,0 +1,11 @@
+﻿namespace KoinoniaHub.API.Dominio.Entidades
+{
+    public static class MeioAceiteTermo
+    {
+        public const string CadastroInicial = "CadastroInicial";
+        public const string PrimeiroAcesso = "PrimeiroAcesso";
+        public const string Login = "Login";
+
+        public static readonly string[] Todos = { CadastroInicial, PrimeiroAcesso, Login };
+    }
+}
